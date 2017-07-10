3374b312ffbbe972996b569ad26ec061a794e02d
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-10 16:17:59 +0200

    Extract the flatten_item part into a more readable private method

--- nested_array.rb
+++ nested_array.rb
@@ -8,13 +8,15 @@ class NestedArray
     return [] if @array.empty?
     flat_array = []
     @array.each do |item|
-      if item.is_a? Array
-        flat_array += NestedArray.new(item).flatten
-      else
-        flat_array << item
-      end
+      flat_array += flatten_item(item)
     end
 
     flat_array
+  end
+
+  private
+
+  def flatten_item(item)
+    item.is_a?(Array) ? NestedArray.new(item).flatten : [item]
   end
 end
