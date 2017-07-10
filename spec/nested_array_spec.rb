6ee40e449ef208a971c6a045fff7a4f2d72d3932
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-10 15:22:03 +0200

    Init nested_array spec

---
+++ spec/nested_array_spec.rb
@@ -0,0 +1,9 @@
+describe NestedArray do
+  let(:array) { [] }
+  subject { described_class.new(array) }
+  describe '#flatten' do
+    context 'when the array is empty' do
+      specify { expect(subject.flatten).to match_array [] }
+    end
+  end
+end
