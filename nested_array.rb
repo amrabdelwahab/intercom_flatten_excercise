24518af6f872212b353ec4e40ca16937b8c1f389
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-10 15:43:59 +0200

    Pass the first test case, return empty array in case one is passed

--- nested_array.rb
+++ nested_array.rb
@@ -1,3 +1,10 @@
+# A class that implements the behavior of a nested array
 class NestedArray
+  def initialize(array)
+    @array = array
+  end
 
+  def flatten
+    []
+  end
 end
--- spec/nested_array_spec.rb
+++ spec/nested_array_spec.rb
@@ -3,6 +3,7 @@ require_relative '../nested_array'
 describe NestedArray do
   let(:array) { [] }
   subject { described_class.new(array) }
+
   describe '#flatten' do
     context 'when the array is empty' do
       specify { expect(subject.flatten).to match_array [] }
